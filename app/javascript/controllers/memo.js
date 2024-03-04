ff0486295defe6635a6d07f7bbe9a843919b8c8d
shiraishono <157781350+shiraishono@users.noreply.github.com>
2024-03-04 14:38:04 +0900

    JSでレスポンスを処理する

--- app/javascript/controllers/memo.js
+++ app/javascript/controllers/memo.js
@@ -1,13 +1,43 @@
-function post (){
+const buildHTML = (XHR) => {
+  const item = XHR.response.post;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.created_at}
+      </div>
+      <div class="post-content">
+        ${item.content}
+      </div>
+    </div>`;
+  return html;
+};
+
+// フォームの送信を処理する関数
+document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("form");
   form.addEventListener("submit", (e) => {
-    e.preventDefault();
+    e.preventDefault(); // ページのリロードを防ぐ
+
     const formData = new FormData(form);
     const XHR = new XMLHttpRequest();
     XHR.open("POST", "/posts", true);
     XHR.responseType = "json";
+
+    // 送信データの設定
+    XHR.onload = () => {
+      if (XHR.status === 200) {
+        const list = document.getElementById("list");
+        const formText = document.getElementById("content");
+        list.insertAdjacentHTML("beforeend", buildHTML(XHR));
+        formText.value = "";
+      } else {
+        alert('エラーが発生しました');
+      }
+    };
+
+    // リクエストの送信
     XHR.send(formData);
   });
-};
-
-window.addEventListener('turbo:load', post);
+});
+// ページの読み込み完了時にpost関数を呼び出す
+window.addEventListener('DOMContentLoaded', post);
